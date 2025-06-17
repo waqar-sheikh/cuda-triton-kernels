f09dc6751dce0c5df06d4075063d2f4b7cce7bc4
Waqar Sheikh <waqar.shk@gmail.com>
2025-06-17 15:30:04 +0200

    Occupancy calculation for triton kernels
    Signed-off-by: Waqar Sheikh <waqar.shk@gmail.com>

--- elementwise/add/add_triton.py
+++ elementwise/add/add_triton.py
@@ -29,8 +29,8 @@ def add_backward(grad_out_ptr, grad_x_ptr, grad_y_ptr, n_elements, BLOCK_SIZE: t
 
 
 def triton_add_forward(x: torch.Tensor, y: torch.Tensor):
-    output = torch.empty_like(x)
     assert x.device == DEVICE and y.device == DEVICE
+    output = torch.empty_like(x)
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
     add_forward[grid](x, y, output, n_elements, BLOCK_SIZE=128)
@@ -38,9 +38,9 @@ def triton_add_forward(x: torch.Tensor, y: torch.Tensor):
 
 
 def triton_add_backward(grad_out: torch.Tensor):
+    assert grad_out.device == DEVICE
     grad_x = torch.empty_like(grad_out)
     grad_y = torch.empty_like(grad_out)
-    assert grad_out.device == DEVICE and grad_x.device == DEVICE and grad_y.device == DEVICE
     n_elements = grad_out.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
     add_backward[grid](grad_out, grad_x, grad_y, n_elements, BLOCK_SIZE=128)
